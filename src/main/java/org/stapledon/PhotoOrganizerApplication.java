da1cdaaf4e213b01f1d1717f31366de818e7d666
Adrian Gilbert <adrian@gilbert.ca>
2022-03-07 21:45:13 -0500

    Add docker-compose to start elasticsearch.

--- src/main/java/org/stapledon/PhotoOrganizerApplication.java
+++ src/main/java/org/stapledon/PhotoOrganizerApplication.java
@@ -1,5 +1,8 @@
 package org.stapledon;
 
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.stapledon.components.PhotoService;
 import org.stapledon.dto.Photo;
 
+import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,10 +19,14 @@ public class PhotoOrganizerApplication {
 	private static final Logger logger = LoggerFactory.getLogger(PhotoOrganizerApplication.class);
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		var ctx = SpringApplication.run(PhotoOrganizerApplication.class, args);
 
+		var elastic = ctx.getBean("restHighLevelClient", RestHighLevelClient.class);
+		logger.info("Elastic: {}", elastic.cluster().health(new ClusterHealthRequest(), RequestOptions.DEFAULT).getClusterName());
+
 		var photoService = ctx.getBean("photoService", PhotoService.class);
+
 		List<Photo> results = photoService.load("R:/Photos/Moments/2013-12-13");
 		logger.info("Loaded {} items", results.size());
 
--- src/main/java/org/stapledon/configuration/Elasticsearch.java
+++ src/main/java/org/stapledon/configuration/Elasticsearch.java
@@ -1,6 +1,10 @@
 package org.stapledon.configuration;
 
+
+import lombok.Getter;
+import lombok.Setter;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -9,11 +13,16 @@ import org.springframework.data.elasticsearch.client.RestClients;
 @Configuration
 public class Elasticsearch {
 
+    @Getter
+    @Setter
+    @Value("${elastic.hostAndPort}")
+    private String hostAndPort;
+
     @Bean
-    public RestHighLevelClient client() {
+    public RestHighLevelClient restHighLevelClient() {
         var clientConfiguration
                 = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(getHostAndPort())
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
